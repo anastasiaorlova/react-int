7601b4cc0d1d97e6f76412cd69ba25e31bf5d0f2
residenttrickster <metkusblackus@gmail.com>
2020-09-16 11:52:54 -0400

    Counter

---
+++ src/Counter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Counter extends React.Component {
+    render()  {
+        return (
+            <div><h1>Counter</h1></div>
+        )
+    }
+}
+
+export default Counter
+
+
+
+
