1dc1b060c59290b992a556f071ae245e088b3140
residenttrickster <metkusblackus@gmail.com>
2020-09-17 19:24:18 -0400

    Test

--- src/Counter.js
+++ src/Counter.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
 class Counter extends React.Component {
+
+    state = {
+        counter: 0
+    }
+
+    handleCounter = () => {
+        this.setState ({
+            counter: this.state.counter+1
+        })
+    }
+
     render()  {
         return (
-            <div><h1>Counter</h1></div>
+            <div><h1>Counter</h1>
+            <button onClick={this.handleCounter}>{this.state.counter} clicked!</button>
+            </div>
         )
     }
 }
