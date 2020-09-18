521f63970c562466ebefd3eab259fcc4374e42d7
residenttrickster <metkusblackus@gmail.com>
2020-09-18 11:04:31 -0400

    Test props

---
+++ src/ParentChild.js
@@ -0,0 +1,56 @@
+//parent component
+
+import React from 'react';
+import Cat from './Cat'
+
+const cats = [
+    {id: 1, name: "Sweety", color: "black"},
+    {id: 2, name: "Snow", color: "white"}
+]
+
+class App extends React.Componentmponent {
+
+    renderCats = () => {
+        const renderedCats = cats.map(cat => {
+            return (
+                <Cat 
+                key={cat.id}
+                name={cat.name}
+                color={cat.color}
+                />
+            )
+        })
+        return renderedCats
+    }
+
+    render() {
+        return (
+            <div className="tiles">
+                {this.renderCats()}
+            </div>
+        )
+    }
+}
+
+export default App;
+
+
+//child component 
+
+
+
+// // tile.js //child
+// import React from 'react'
+
+// class Tile extends React.Component {
+//   render() {
+//       return( 
+//       <div className="tile">
+//       <h1>{this.props.name}</h1>
+//       <p>{this.props.toy}</p>
+//     </div>
+//     )
+//   }
+// }
+
+// export default Tile
